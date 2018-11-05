514307a523be98385fbcde58659941dd8f0def96
Alexandros Tsakiris <alexandros.tsakiris@hotmail.com>
2018-11-06 01:04:52 +0200

    Δημιουργία layout ενδεικτικών κατηγοριών Δημιουργία λειτουργίας εμφάνισης αποτελεσμάτων και ενσωματωμένου YouTube Player με API (μη τελειωμένο).

---
+++ app/src/main/java/com/example/aggelos/digitalaristotle/PlayerActivity.java
@@ -0,0 +1,15 @@
+package com.example.aggelos.digitalaristotle;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.aggelos.myapplication.R;
+
+public class PlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+    }
+}
